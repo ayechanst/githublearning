e8d6257ec433d1a4e715ae84997b8182a37f8d4b
Aye Chan <ayechan.sant@gmail.com>
2023-07-16 15:25:59 -0700

    different branch

--- testGitFile.js
+++ testGitFile.js
@@ -1,3 +1,4 @@
 console.log("This is my initial commit no cap");
 console.log("This is a change");
 console.log("Making sure I can push normally");
+console.log("This change should be on the new branch, and not on the main one.")
