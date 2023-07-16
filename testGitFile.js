c5025acfdb4c1b63c2de71f26337c1ef35dd6236
Aye Chan <ayechan.sant@gmail.com>
2023-07-16 15:51:14 -0700

    last update on test branch

--- testGitFile.js
+++ testGitFile.js
@@ -2,3 +2,4 @@ console.log("This is my initial commit no cap");
 console.log("This is a change");
 console.log("Making sure I can push normally");
 console.log("This change should be on the new branch, and not on the main one.")
+console.log("adding one more change before getting ready to merge")
