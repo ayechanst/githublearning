f1948e6bdab0cbfacd8bcb53f8b8e2daeec8144d
Aye Chan <ayechan.sant@gmail.com>
2023-07-16 15:48:38 -0700

    commiting on the main branch

--- testGitFile.js
+++ testGitFile.js
@@ -1,3 +1,4 @@
 console.log("This is my initial commit no cap");
 console.log("This is a change");
 console.log("Making sure I can push normally");
+console.log("wow now i am working on the main branch.")
